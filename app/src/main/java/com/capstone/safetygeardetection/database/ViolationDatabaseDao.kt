b4c64c1c4a90a97fce2b7ae5e35863345a6476a6
clarkmyfancy <clark.jack0@gmail.com>
2019-11-03 16:38:48 -0600

    added implementations of database and databasedao

--- app/src/main/java/com/capstone/safetygeardetection/database/ViolationDatabaseDao.kt
+++ app/src/main/java/com/capstone/safetygeardetection/database/ViolationDatabaseDao.kt
@@ -1,1 +1,29 @@
 package com.capstone.safetygeardetection.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface ViolationDatabaseDao {
+
+    @Insert
+    fun insert(violation: Violation)
+
+    @Update
+    fun update(violation: Violation)
+
+    @Query("SELECT * FROM violation_table WHERE violationID = :key")
+    fun getViolation(key: Long): Violation?
+
+    @Query("SELECT * FROM violation_table")
+    fun getAllViolations(): LiveData<List<Violation>>
+
+    @Query("SELECT * FROM violation_table ORDER BY violationID DESC")
+    fun getMostRecentViolation(): Violation?
+
+    @Query("DELETE FROM violation_table WHERE violationID = :key")
+    fun deleteViolation(key: Long): Unit
+}
